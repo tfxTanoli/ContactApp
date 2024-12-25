cb27a9dd27b20679327731f47a5bfca3c4c21546
Muhammad Usman <usman5194999@gmail.com>
2024-12-25 16:46:04 +0500

    Solving Quiz Question

--- lib/main.dart
+++ lib/main.dart
@@ -8,61 +8,117 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AddContact(),
+      home: ShowListView(),
     );
   }
 }
 
-class Contact {
+class Info {
   String? name;
-  String? phoneNo;
-  Contact({this.name, this.phoneNo});
+  int? age;
+  String? city;
+
+  Info({this.name, this.age, this.city});
 }
 
-class AddContact extends StatefulWidget {
-  _AddContact createState() => _AddContact();
+class ShowListView extends StatefulWidget {
+  _ShowListView createState() => _ShowListView();
 }
 
-class _AddContact extends State<AddContact> {
-  List<Contact> contact = [
-    Contact(name: "Muhammad Usman", phoneNo: "0346 5617010"),
-    Contact(name: "Luqman", phoneNo: "0316 9205375")
+class _ShowListView extends State<ShowListView> {
+  List<Info> info = [
+    Info(name: "Noman", age: 23, city: "Karachi"),
+    Info(name: 'Faisal', age: 53, city: "Lahore"),
+    Info(name: 'Ali', age: 33, city: "RawalPindi")
   ];
-  void addContact() {
-    setState(() {
-      contact.add(Contact(
-          name: "New Contact ${contact.length + 1}",
-          phoneNo: "0300 000000${contact.length + 1}"));
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Contact Application"),
+        title: const Text("Showing Data In List View"),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Expanded(
-              child: ListView.builder(
-                itemCount: contact.length,
-                itemBuilder: (context, index) => ListTile(
-                  title: Text(contact[index].name.toString()),
-                  subtitle: Text(contact[index].phoneNo.toString()),
-                ),
+                child: ListView.builder(
+              itemCount: info.length,
+              itemBuilder: (context, index) => ListTile(
+                title: Text("Name ${info[index].name}"),
+                subtitle:
+                    Text("Age : ${info[index].age}  City ${info[index].city}"),
               ),
-            )
+            ))
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: addContact,
-        child: const Icon(Icons.add),
       ),
     );
   }
 }
 
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: AddContact(),
+//     );
+//   }
+// }
+
+// class Contact {
+//   String? name;
+//   String? phoneNo;
+//   Contact({this.name, this.phoneNo});
+// }
+
+// class AddContact extends StatefulWidget {
+//   _AddContact createState() => _AddContact();
+// }
+
+// class _AddContact extends State<AddContact> {
+//   List<Contact> contact = [
+//     Contact(name: "Muhammad Usman", phoneNo: "0346 5617010"),
+//     Contact(name: "Luqman", phoneNo: "0316 9205375")
+//   ];
+//   void addContact() {
+//     setState(() {
+//       contact.add(Contact(
+//           name: "New Contact ${contact.length + 1}",
+//           phoneNo: "0300 000000${contact.length + 1}"));
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Contact Application"),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Expanded(
+//               child: ListView.builder(
+//                 itemCount: contact.length,
+//                 itemBuilder: (context, index) => ListTile(
+//                   title: Text(contact[index].name.toString()),
+//                   subtitle: Text(contact[index].phoneNo.toString()),
+//                 ),
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: addContact,
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
