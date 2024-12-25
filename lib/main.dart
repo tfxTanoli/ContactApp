6db5ee082c8329c2b269d1b9c3e377cbe3ef1432
Muhammad Usman <usman5194999@gmail.com>
2024-12-25 17:58:02 +0500

    Quiz Practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -8,54 +9,157 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ShowListView(),
+      home: TextControllerDemo(),
     );
   }
 }
 
-class Info {
-  String? name;
-  int? age;
-  String? city;
-
-  Info({this.name, this.age, this.city});
+class TextControllerDemo extends StatefulWidget {
+  _TextControllerDemo createState() => _TextControllerDemo();
 }
 
-class ShowListView extends StatefulWidget {
-  _ShowListView createState() => _ShowListView();
-}
+class _TextControllerDemo extends State<TextControllerDemo> {
+  int number = 0;
+  void handle() {
+    if (int.tryParse(A.text)! > 5 ||
+        int.tryParse(B.text)! > 5 ||
+        int.tryParse(C.text)! > 5) {
+      A.text = '0';
+      B.text = '0';
+      C.text = '0';
+    }
+    number = Random().nextInt(9) + 1;
+    setState(() {
+      if (number >= 1 && number <= 3) {
+        A.text = (int.tryParse(A.text)! + 1).toString();
+      } else if (number >= 4 && number <= 6) {
+        B.text = (int.parse(B.text) + 1).toString();
+      } else {
+        C.text = (int.parse(C.text) + 1).toString();
+      }
+    });
+  }
 
-class _ShowListView extends State<ShowListView> {
-  List<Info> info = [
-    Info(name: "Noman", age: 23, city: "Karachi"),
-    Info(name: 'Faisal', age: 53, city: "Lahore"),
-    Info(name: 'Ali', age: 33, city: "RawalPindi")
-  ];
+  TextEditingController A = TextEditingController(text: '0');
+  TextEditingController B = TextEditingController(text: '0');
+  TextEditingController C = TextEditingController(text: '0');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Showing Data In List View"),
+        title: const Text("Text Editing Controller Demo"),
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Expanded(
-                child: ListView.builder(
-              itemCount: info.length,
-              itemBuilder: (context, index) => ListTile(
-                title: Text("Name ${info[index].name}"),
-                subtitle:
-                    Text("Age : ${info[index].age}  City ${info[index].city}"),
+            Text(
+              "Random Number Generated Is $number",
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: A,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: '0',
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: TextField(
+                    controller: B,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: '0',
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: TextField(
+                    controller: C,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: '0',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: handle,
+              child: const Center(
+                child: Text("Click Here"),
               ),
-            ))
+            )
           ],
         ),
       ),
     );
   }
 }
+
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: ShowListView(),
+//     );
+//   }
+// }
+
+// class Info {
+//   String? name;
+//   int? age;
+//   String? city;
+
+//   Info({this.name, this.age, this.city});
+// }
+
+// class ShowListView extends StatefulWidget {
+//   _ShowListView createState() => _ShowListView();
+// }
+
+// class _ShowListView extends State<ShowListView> {
+//   List<Info> info = [
+//     Info(name: "Noman", age: 23, city: "Karachi"),
+//     Info(name: 'Faisal', age: 53, city: "Lahore"),
+//     Info(name: 'Ali', age: 33, city: "RawalPindi")
+//   ];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Showing Data In List View"),
+//       ),
+//       body: Center(
+//         child: Column(
+//           children: [
+//             Expanded(
+//                 child: ListView.builder(
+//               itemCount: info.length,
+//               itemBuilder: (context, index) => ListTile(
+//                 title: Text("Name ${info[index].name}"),
+//                 subtitle:
+//                     Text("Age : ${info[index].age}  City ${info[index].city}"),
+//               ),
+//             ))
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 // void main() {
 //   runApp(MyApp());
